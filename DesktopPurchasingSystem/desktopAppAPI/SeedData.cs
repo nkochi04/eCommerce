807a83ee3599e31b89d0ede874c40d9625176cb9
nk031u <nicolas.koch@linde.com>
2024-05-06 15:03:04 +0200

    added realistic prices

--- DesktopPurchasingSystem/desktopAppAPI/SeedData.cs
+++ DesktopPurchasingSystem/desktopAppAPI/SeedData.cs
@@ -186,21 +186,23 @@ namespace DesktopAppAPI
 
         private static void InitializeProducts(desktopAppDbContext _db)
         {
-            List<string> products = ["Computer", "Printer", "Telephone", "Fax machine", "Scanner", "Copier", "Monitor", "Keyboard", "Mouse", "Laptop", "Projector", "Power supply", "Iron for shirts", "Floor lamp", "Desk lamp", "Paper shredder", "Radio alarm clock", "USB hub", "Webcam", "Headset", "Speakers", "Mobile phone charger", "Tablet", "Smartboard", "Remote control", "Batteries", "Power cable", "Magnetic whiteboard", "Stapler", "Staple remover", "Letter scale", "Space heater", "Humidifier", "Iron for documents", "HDMI cable", "VGA cable", "DVI cable", "DisplayPort cable", "Ethernet cable", "USB cable", "Replacement ink cartridges", "Replacement toner cartridges", "Office chair with massage function", "Footrest", "Laptop stand", "Desk fan", "Water dispenser", "Coffee machine"];
+            Dictionary<string, float> productPrices = new() { { "Computer", 1200.49f }, { "Printer", 149.99f }, { "Telephone", 79.99f }, { "Fax machine", 199.99f }, { "Scanner", 84.99f }, { "Copier", 299.99f }, { "Monitor", 249.99f }, { "Keyboard", 49.99f }, { "Mouse", 19.99f }, { "Laptop", 1499.99f }, { "Projector", 799.99f }, { "Power supply", 59.99f }, { "Iron for shirts", 34.99f }, { "Floor lamp", 44.99f }, { "Desk lamp", 29.99f }, { "Paper shredder", 64.99f }, { "Radio alarm clock", 24.99f }, { "USB hub", 14.99f }, { "Webcam", 69.99f }, { "Headset", 49.99f }, { "Speakers", 74.99f }, { "Mobile phone charger", 14.99f }, { "Tablet", 299.99f }, { "Smartboard", 1499.99f }, { "Remote control", 19.99f }, { "Batteries", 9.99f }, { "Power cable", 9.99f }, { "Magnetic whiteboard", 149.99f }, { "Stapler", 9.99f }, { "Staple remover", 4.99f }, { "Letter scale", 24.99f }, { "Space heater", 59.99f }, { "Humidifier", 39.99f }, { "Iron for documents", 34.99f }, { "HDMI cable", 14.99f }, { "VGA cable", 14.99f }, { "DVI cable", 14.99f }, { "DisplayPort cable", 19.99f }, { "Ethernet cable", 9.99f }, { "USB cable", 4.99f }, { "Replacement ink cartridges", 29.99f }, { "Replacement toner cartridges", 59.99f }, { "Office chair with massage function", 499.99f }, { "Footrest", 29.99f }, { "Laptop stand", 24.99f }, { "Desk fan", 29.99f }, { "Water dispenser", 99.99f }, { "Coffee machine", 149.99f } };
 
             List<SellerModel> seller = [.. _db.Sellers];
 
-            for (int i = 0; i < products.Count; i++)
+            Random random = new();
+
+            foreach (var product in productPrices)
             {
                 _db.Products.Add(
                     new ProductModel
                     {
                         ID = Guid.NewGuid(),
-                        Seller_ID = seller[i % 3].ID,
-                        Name = products[i],
-                        PiecesAvailable = 50,
-                        Price = 35,
-                        ImageData = File.ReadAllBytes($"Images/{products[i].Replace(" ", string.Empty).ToLower()}.jpg")
+                        Seller_ID = seller[random.Next(0, 2)].ID,
+                        Name = product.Key, // product name
+                        PiecesAvailable = 10,
+                        Price = product.Value, // product price
+                        ImageData = File.ReadAllBytes($"Images/{product.Key.Replace(" ", string.Empty).ToLower()}.jpg")
                     }
                 );
             }
@@ -209,12 +211,11 @@ namespace DesktopAppAPI
         private static void InitializeProductSerialNumber(desktopAppDbContext _db)
         {
             Random random = new();
-            int randomNumber;
             foreach (var product in _db.Products)
             {
                 for (int i = 0; i < 10; i++)
                 {
-                    randomNumber = random.Next(100000000, 1000000000);
+                    var randomNumber = random.Next(100000000, 1000000000);
                     _db.Add(new PieceModel { Product_ID = product.ID, Serial_Number = randomNumber, Sold = false });
                 }
             }
